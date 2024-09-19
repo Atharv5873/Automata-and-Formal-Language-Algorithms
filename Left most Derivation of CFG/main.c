77b659a690dcf91a430e1cf865a4197667b57377
Atharv <atharv5873@gmail.com>
2024-09-19 12:36:45 +0530

    Initialize basic structure for CFG Derivation with Production rule definition

---
+++ Left most Derivation of CFG/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_PRODUCTIONS 50
+#define MAX_LENGTH 50
+
+typedef struct Production{
+    char lhs;
+    char rhs[MAX_LENGTH];
+};
+
+int main()
+{
+
+}
